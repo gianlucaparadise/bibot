551bf865833958f4927664fcb5b258959b462935
g.paradiso <g.paradiso@accenture.com>
2017-02-05 17:15:18 +0100

    Registration commented

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,24 @@ var router = express.Router();
 
 var botgram = require("botgram");
 
+var bot = botgram(process.env.TELEGRAM_TOKEN);
+
+bot.command("start", function (msg, reply, next) {
+  console.log("Start from: ", msg.from.id);
+  reply.text("Ciao! Sono Bibot.");
+});
+
+bot.command("stop", function (msg, reply, next) {
+  console.log("Stopped from: ", msg.from.id);
+  reply.text("Ciao, a presto!");
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Bibot' });
 });
 
-router.get('/register', function(req, res, next) {
-  var bot = botgram(process.env.TELEGRAM_TOKEN);
+/*router.get('/register', function (req, res, next) {
 
   bot.command("start", function (msg, reply, next) {
     console.log("Start from: ", msg.from.id);
@@ -17,6 +28,6 @@ router.get('/register', function(req, res, next) {
   });
 
   res.render('register', { title: 'Bibot', body: 'Registration completed' });
-});
+});*/
 
 module.exports = router;
