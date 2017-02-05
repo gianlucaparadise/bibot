688d2d8dddebdc3248562b9db8b5b7b3d769bb7e
g.paradiso <g.paradiso@accenture.com>
2017-02-05 16:47:14 +0100

    Bot init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var botgram = require("botgram");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bibot' });
+});
+
+router.get('/register', function(req, res, next) {
+  var bot = botgram(process.env.TELEGRAM_TOKEN);
+
+  bot.command("start", function (msg, reply, next) {
+    console.log("Ciao!", msg.from.id);
+  });
+
+  res.send("Register completed");
 });
 
 module.exports = router;
