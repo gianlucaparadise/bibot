b263d3ca8ecad45eb28e1773959ad6d138791521
g.paradiso <g.paradiso@accenture.com>
2017-06-23 19:36:18 +0200

    env var not found: fix

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//connect to DB
-mongoose.Promise = global.Promise;
-console.log("db url: " + process.env.DB_URL);
-mongoose.connect(process.env.DB_URL);
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -49,6 +43,12 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//connect to DB
+mongoose.Promise = global.Promise;
+console.log("db url: " + process.env.DB_URL);
+mongoose.connect(process.env.DB_URL);
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
 require('./bot'); // this is bot source code
 //require('./utils/keepAwake'); // to keep heroku awake
