9d71b1439a617952eb3f1974bfcfd81742d3e368
g.paradiso <g.paradiso@accenture.com>
2017-06-24 19:23:36 +0200

    test calendar: addDays method

--- bot/calendar-telegram-nodejs/index.js
+++ bot/calendar-telegram-nodejs/index.js
@@ -2,20 +2,53 @@ const Extra = require('telegraf').Extra;
 
 module.exports = {
 	getCalendar: function () {
-		return Extra.HTML().markup((m) =>
-			m.inlineKeyboard([
-				[m.callbackButton("1", "calendar-1"), m.callbackButton("2", "calendar-2")],
-				[m.callbackButton("3", "calendar-3"), m.callbackButton("4", "calendar-4")]
-			])
-		);
+		return Extra.HTML().markup((m) => {
+			let page = [];
+			addDays(page);
+			// m.inlineKeyboard([
+			// 	[m.callbackButton("1", "calendar-telegram-1"), m.callbackButton("2", "calendar-telegram-2")],
+			// 	[m.callbackButton("3", "calendar-telegram-3"), m.callbackButton("4", "calendar-telegram-4")]
+			// ]);
+			m.inlineKeyboard(page);
+		});
 	},
 
 	setDateListener: function (bot, onDateSelected) {
-		bot.action(/calendar-\d+/g, context => {
+		bot.action(/calendar-telegram-\d+/g, context => {
 			if (onDateSelected) {
-				let date = context.match[0];
+				let date = context.match[0].replace("calendar-telegram-", "");
 				onDateSelected(context, date);
 			}
 		});
 	}
+}
+
+function addDays(page, m) {
+	let date = new Date();
+	let maxDays = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+	let currentRow = new Array(7);
+	for (var d = 1; d <= maxDays; d++) {
+		date.setDate(d);
+
+		let weekDay = date.getDay();
+		//currentRow[weekDay] = toYyyymmdd(date);
+		currentRow[weekDay] = m.callbackButton(d.toString(), "calendar-telegram-" + toYyyymmdd(date));
+
+		if (weekDay == 6 || d == maxDays) {
+			page.push(currentRow);
+			currentRow = new Array(7);
+		}
+	}
+}
+
+function toYyyymmdd(date) {
+	let mm = date.getMonth() + 1; // getMonth() is zero-based
+	let dd = date.getDate();
+
+	return [
+		date.getFullYear(),
+		(mm > 9 ? '' : '0') + mm,
+		(dd > 9 ? '' : '0') + dd
+	].join('');
 }
