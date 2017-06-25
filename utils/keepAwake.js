8dec1a9d3ddede94e54c20af3a4beafd9faa136e
g.paradiso <g.paradiso@accenture.com>
2017-06-25 15:34:27 +0200

    keepAwake remove

--- utils/keepAwake.js
+++
@@ -1,5 +0,0 @@
-const http = require("http");
-setInterval(function () {
-	http.get("http://bibot-telegram.herokuapp.com");
-	console.log("Wake up, Bibot!");
-}, 300000); // every 5 minutes (300000)
